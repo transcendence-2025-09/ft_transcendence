616c317bf4ba60a3e1c8454a4e3366f349272ec1
syamasaw <138424335+Sawawa42@users.noreply.github.com>
2025-09-25 12:15:54 +0900

    biomeの設定変更 (#14)

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,29 +1,29 @@
 import Fastify, { type FastifyInstance, type FastifyRequest } from "fastify";
 
 async function routes(fastify: FastifyInstance) {
-	fastify.get("/", async (request: FastifyRequest) => {
-		return { hello: "world" };
-	});
+  fastify.get("/", async (request: FastifyRequest) => {
+    return { hello: "world" };
+  });
 }
 
 async function buildApp(): Promise<FastifyInstance> {
-	const fastify: FastifyInstance = Fastify({
-		logger: true,
-	});
+  const fastify: FastifyInstance = Fastify({
+    logger: true,
+  });
 
-	fastify.register(routes);
+  fastify.register(routes);
 
-	return fastify;
+  return fastify;
 }
 
 async function start(): Promise<void> {
-	try {
-		const app: FastifyInstance = await buildApp();
-		await app.listen({ port: 3000 });
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
+  try {
+    const app: FastifyInstance = await buildApp();
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 start();
