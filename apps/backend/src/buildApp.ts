6de344641560dbcbd5aec447b7cb0e7608325b9e
syamasaw <138424335+Sawawa42@users.noreply.github.com>
2025-09-28 01:09:48 +0900

    refactor(misc): 不要なファイルの削除 (#43)
    * healthの削除
    
    * counterの削除(init時のデモ)

--- apps/backend/src/buildApp.ts
+++ apps/backend/src/buildApp.ts
@@ -1,13 +1,11 @@
 import Fastify, { type FastifyInstance } from "fastify";
 import { authRoute } from "./auth/authRoute.js";
-import { healthRoute } from "./health/index.js";
 
 export async function buildApp(): Promise<FastifyInstance> {
   const fastify: FastifyInstance = Fastify({
     logger: true,
   });
 
-  fastify.register(healthRoute);
   fastify.register(authRoute);
 
   return fastify;
