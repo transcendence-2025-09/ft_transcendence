e54f88839635aee507b448232e6c225a09f1712c
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2025-10-10 14:35:44 +0900

    PongGameクラスの作成と実装

--- apps/frontend/src/pages/pong.ts
+++ apps/frontend/src/pages/pong.ts
@@ -2,19 +2,46 @@ import { pongBackGroundFactory } from "../components/pong/pongBackGround";
 import { pongGame } from "../components/pong/pongGame";
 import { pongHeader } from "../components/pong/pongHeader";
 import { pongPlayerRow } from "../components/pong/pongPlayerRow";
+import type { ElComponent } from "../factory/componentFactory";
 import { pageFactory } from "../factory/pageFactory";
+import type { RenderOption } from "../logic/pong/pongGame";
+import { PongGame } from "../logic/pong/pongGame";
 
 //pong pageを積むためのbackground
-const pongBg = pongBackGroundFactory();
+const pongPageComp = (): ElComponent => {
+  const bg = pongBackGroundFactory();
+  const content = bg.el.querySelector("#pong-container") as HTMLDivElement;
 
-//pongBgの中にあるコンテナ
-const content = pongBg.el.querySelector("#pong-container") as HTMLDivElement;
-const header = pongHeader();
-const playerLow = pongPlayerRow();
-const canvas = pongGame();
+  const header = pongHeader();
+  const playerLow = pongPlayerRow();
+  const pong = pongGame();
 
-header.mount(content);
-playerLow.mount(content);
-canvas.mount(content);
+  let gameInstance: PongGame | null = null;
 
-export const pongPage = pageFactory([pongBg]);
+  const opt: RenderOption | null = null;
+
+  return {
+    el: bg.el,
+    mount(target, anchor = null) {
+      bg.mount(target, anchor);
+      header.mount(content);
+      playerLow.mount(content);
+      pong.mount(content);
+      const canvas = pong.el as HTMLCanvasElement;
+      gameInstance = new PongGame(canvas, opt);
+      gameInstance.registerKeyEvent();
+      gameInstance.start();
+    },
+    unmount() {
+      if (gameInstance) gameInstance.stop();
+      gameInstance?.unregisterKeyEvent();
+      gameInstance = null;
+      pong.unmount();
+      playerLow.unmount();
+      header.unmount();
+      bg.unmount();
+    },
+  };
+};
+
+export const pongPage = pageFactory([pongPageComp()]);
