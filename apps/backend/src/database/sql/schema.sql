1ab4e7fdd4ba952769ae27684b5e8a8f64da86b0
syamasaw <138424335+Sawawa42@users.noreply.github.com>
2025-10-14 20:02:22 +0900

    テーブルの追加 (#134)

--- apps/backend/src/database/sql/schema.sql
+++ apps/backend/src/database/sql/schema.sql
@@ -5,3 +5,29 @@ CREATE TABLE IF NOT EXISTS users (
   ft_id INTEGER UNIQUE,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS tournaments (
+  id TEXT NOT NULL PRIMARY KEY, -- UUID?
+  name TEXT NOT NULL,
+  host_id INTEGER NOT NULL,
+  winner_id INTEGER,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (host_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS matches (
+  id TEXT NOT NULL PRIMARY KEY, -- UUID?
+  tournament_id TEXT NOT NULL,
+  round INTEGER NOT NULL, -- 1回戦なら1, 2回戦なら2など
+  player1_id INTEGER NOT NULL,
+  player2_id INTEGER NOT NULL,
+  player1_score INTEGER NOT NULL,
+  player2_score INTEGER NOT NULL,
+  winner_id INTEGER, -- スコアから判断できるが、クエリを簡単にするため
+  played_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (tournament_id) REFERENCES tournaments(id),
+  FOREIGN KEY (player1_id) REFERENCES users(id),
+  FOREIGN KEY (player2_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id)
+);
