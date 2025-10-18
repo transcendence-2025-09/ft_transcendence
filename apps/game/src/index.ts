1c03b41506aa694f8a1f0b8b918ef2f76c160b98
rmatsuba <ryutaro140515@icloud.com>
2025-10-19 04:05:18 +0900

    pongのロジック部分をGame serverに移植。挙動テスト済み (#161)
    frontのPongGameにまだコードが残っていますが、フロント側の実装が終わってないのでそれはまた別ブランチで実装して消します。なので気にしないでください。

--- apps/game/src/index.ts
+++ apps/game/src/index.ts
@@ -1,13 +1,18 @@
 import Fastify, { type FastifyInstance } from "fastify";
+import { pongLogic } from "./pong";
+
 import "dotenv/config";
 
 const app: FastifyInstance = Fastify({ logger: true });
-
-app.get("/", (_req, rep) => {
-  // console.log(req);
-  console.log(process.env.NODE_ENV);
-  rep.send("Hello from Game server");
+app.register(import("@fastify/websocket"));
+app.register(require("@fastify/http-proxy"), {
+  upstream: "http://localhost:3000",
+  prefix: "/api",
+  http2: false,
 });
+app.register(pongLogic);
+
+app.get("/", async () => ({ message: "Hello from game server" }));
 
 try {
   app.listen({ port: 3001, host: "0.0.0.0" });
