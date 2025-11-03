a1ff62b843c22325d7cbcacf70b181d5ea7b3fd2
syamasaw <seitaro.yamasawa@3-shake.com>
2025-11-03 17:13:02 +0900

    seedのトーナメントのuuidを修正

--- apps/backend/src/database/sql/seed.sql
+++ apps/backend/src/database/sql/seed.sql
@@ -5,12 +5,12 @@ INSERT OR IGNORE INTO users (id, name, email, ft_id) VALUES
 (1003, "Bob Brown", "bob.brown@example.com", 1003);
 
 INSERT OR IGNORE INTO tournaments (id, name, host_id, winner_id) VALUES
-("2f4d2306-ca5e-8145-7eb1-4b58773e466e", "AAAAA", 1000, 1000); -- uid1000主催、uid1000優勝
+("019a48c3-e038-7024-8530-662c19e53424", "AAAAA", 1000, 1000); -- uid1000主催、uid1000優勝
 
 INSERT OR IGNORE INTO matches (id, tournament_id, round, player1_id, player2_id, player1_score, player2_score, winner_id) VALUES
-("019a43ec-b8cf-75b9-b590-b8f1be43320f", "2f4d2306-ca5e-8145-7eb1-4b58773e466e", 1, 1000, 1001, 3, 2, 1000), -- 1回戦 1000 vs 1001, 3-2で1000の勝ち
-("019a43ed-0040-78b0-8c44-8ee2cdcb9b9e", "2f4d2306-ca5e-8145-7eb1-4b58773e466e", 1, 1002, 1003, 1, 3, 1003), -- 1回戦 1002 vs 1003, 1-3で1003の勝ち
-("019a43eb-9e07-7b76-a523-8c1c6cd09c73", "2f4d2306-ca5e-8145-7eb1-4b58773e466e", 2, 1000, 1003, 3, 0, 1000); -- 決勝(2回戦) 1000 vs 1003, 3-0で1000の勝ち
+("019a43ec-b8cf-75b9-b590-b8f1be43320f", "019a48c3-e038-7024-8530-662c19e53424", 1, 1000, 1001, 3, 2, 1000), -- 1回戦 1000 vs 1001, 3-2で1000の勝ち
+("019a43ed-0040-78b0-8c44-8ee2cdcb9b9e", "019a48c3-e038-7024-8530-662c19e53424", 1, 1002, 1003, 1, 3, 1003), -- 1回戦 1002 vs 1003, 1-3で1003の勝ち
+("019a43eb-9e07-7b76-a523-8c1c6cd09c73", "019a48c3-e038-7024-8530-662c19e53424", 2, 1000, 1003, 3, 0, 1000); -- 決勝(2回戦) 1000 vs 1003, 3-0で1000の勝ち
 
 INSERT OR IGNORE INTO user_stats (id, user_id, average_score, number_of_matches, number_of_wins, current_winning_streak, total_score_points, total_loss_points, last_match_id) VALUES
 ("019a43e6-4b5a-7753-8d32-f69d893d8d0a", 1000, 3.0, 2, 2, 2, 6, 2, "019a43eb-9e07-7b76-a523-8c1c6cd09c73"), -- uid1000の統計情報
